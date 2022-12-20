51b22b3b5c94985d4f6b98e403a37c793ff575b1
naseokju <lukas95@naver.com>
2022-12-20 17:42:41 +0900

    정적 파일 접근

--- src/index.server.js
+++ src/index.server.js
@@ -1,11 +1,42 @@
 import ReactDOMServer from "react-dom/server";
 import express from "express";
 import { StaticRouter } from "react-router-dom/server";
+
 import App from "./App";
+import path from "path";
+import fs from "fs";
+
+// asset-manifest.json에서 파일 경로들을 조회함
+const manifest = JSON.parse(
+    fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
+);
+
+function createPage(root) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1,shrink-to-fit=no"
+            />
+            <meta name="theme-color" content="#000000" />
+            <title>React App</title>           
+            <link href="${manifest.files['main.css']}" rel="stylesheet" />
+        </head>
+        <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+            <div id="root">${root}</div>
+            <script src="${manifest.files['main.js']}"></script>
+        </body>
+    </html>
+    `;
+} 
 
 const app = express();
 
-// 서버 사이드 렌더리을 처리할 핸들러 함수
+// 서버 사이드 렌더링을 처리할 핸들러 함수
 const serverRender = (req, res, next) => {
     // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해줌
     
@@ -16,9 +47,14 @@ const serverRender = (req, res, next) => {
         </StaticRouter>
     );
     const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-    res.send(root); // 클라이언트에게 결과물을 응답
+    res.send(createPage(root)); // 클라이언트에게 결과물을 응답
 };
 
+const serve = express.static(path.resolve('./build'), {
+    index: false // "/" 경로에서 index.html을 보여 주지 않도록 설정
+});
+
+app.use(serve); // 순서 중요. serverRender 전에 위치
 app.use(serverRender);
 
 // 5002 포트로 서버 가동
