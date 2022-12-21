f0130fa5ae276cc8f06c29029fd5eae0c502b3e0
naseokju <lukas95@naver.com>
2022-12-21 16:39:06 +0900

    redux-saga

--- src/modules/users.js
+++ src/modules/users.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import { call, put, takeEvery } from "redux-saga/effects";
 
 // 액션 타입 정의
 const GET_USERS_PENDING = 'users/GET_USERS_PENDING';
 const GET_USERS_SUCCESS = 'users/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'users/GET_USERS_FAILURE';
+
+const GET_USER = 'users/GET_USER';
+const GET_USER_SUCCESS = 'users/GET_USER_SUCCESS';
+const GET_USER_FAILURE = 'users/GET_USER_FAILURE';
 
 // 액션 생성 함수 만들기
 const getUsersPending = () => ({ type: GET_USERS_PENDING });
@@ -27,6 +32,31 @@ export const getUsers = () => async dispatch => {
         throw e;
     }
 };
+
+// saga 함수 생성
+export const getUser = id => ({ type: GET_USER, payload: id });
+const getUserSuccess = data => ({ type: GET_USER_SUCCESS, payload: data });
+const getUserFailure = error => ({
+    type: GET_USER_FAILURE,
+    payload: error,
+    error: true
+});
+
+const getUserById = id =>
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+function* getUserSaga(action) {
+    try {
+        const response = yield call(getUserById, action.payload);
+        yield put(getUserSuccess(response.data));
+    } catch (e) {
+        yield put(getUserFailure(e));
+    }
+}
+
+export function* usersSaga() {
+    yield takeEvery(GET_USER, getUserSaga);
+}
 
 // 초기 상태 및 리듀서 함수 만들기
 const initialState = {
@@ -60,6 +90,24 @@ function users(state = initialState, action) {
                 ...state,
                 loading: { ...state.loading, users: false },
                 error: { ...state.error, users: action.payload }
+            };
+        case GET_USER:
+            return {
+                ...state,
+                loading: { ...state.loading, user: true },
+                error: { ...state.error, user: null }
+            };
+        case GET_USER_SUCCESS:
+            return {
+                ...state,
+                loading: { ...state.loading, user: false },
+                user: action.payload
+            };
+        case GET_USER_FAILURE:
+            return {
+                ...state,
+                loading: { ...state.loading, user: false },
+                error: { ...state.error, user: action.payload }
             };
         default:
             return state;
