cab85be1e33d239496e3719d7b8a5a87a0e3dde5
naseokju <lukas95@naver.com>
2022-12-21 16:38:39 +0900

    redux-saga

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer, 
   // 브라우저에서 상태를 재사용하기 위해 스토어 생성 과정에서
   window.__PRELOADED__STATE__, // 이 값을 초기 상태로 사용함
-  applyMiddleware(thunk));
+  applyMiddleware(thunk, sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
-import users from "./users";
+import { all } from "redux-saga/effects";
+import users, { usersSaga } from "./users";
+
+// 루트 사가 만들기
+export function* rootSaga() {
+    yield all([usersSaga()]);
+}
 
 // 루트 리듀서를 만들기
 const rootReducer = combineReducers({ users });
